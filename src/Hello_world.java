d72002157eabd70f3b669519bab81acc8ca92240
DhineshKaviyarasu <dhlipdhinesh710@gmail.com>
2023-09-07 09:18:50 +0530

    Initial commit

---
+++ src/Hello_world.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Hello_world {
+    public static void main(String[] args) {
+        int[] num=new int[5];
+        num[0]=1;
+        num[1]=1;
+        num[2]=1;
+        num[3]=1;
+        num[4]=1;
+
+
+        System.out.println(num[0]);
+
+    }
+}
